7d2f90eabcaefcb99d4968693fe6e0098069bc0e
bad-frontend-dev <diamondcrowncob@gmail.com>
2024-11-20 01:09:37 +0000

    scroll thing

--- client/src/events/listeners/handlers/scroll.ts
+++ client/src/events/listeners/handlers/scroll.ts
@@ -1,26 +1,29 @@
-import { fetchMessages, prependMessages } from "../../../utils"
-import { $messages } from "../../../globals"
+import { fetchMessages, prependMessages } from "../../../utils";
+import { $messages } from "../../../globals";
 
 //let loadingMessages = false
-let loadedMessages = 50
-let timeout = null
+let loadedMessages = 50;
+let timeout = null;
 
 export function scrollHandler(event: JQuery.ScrollEvent) {
-	if (!timeout) {
-		timeout = setTimeout(() => {
-			clearTimeout(timeout)
-			timeout = null
-			if (this.scrollTop < 600) {
-			  	  const currentHeight = $messages[0].scrollHeight
-				    const currentScroll = $messages.scrollTop()
-          prependMessages("public", 50, loadedMessages).then(messages => {
-				    const newHeight = $messages[0].scrollHeight
-          
-				    $messages.scrollTop(newHeight - currentHeight + currentScroll)
-				    loadedMessages += messages.length
-        })
-      }
-			}, 250)
-		}
-	}
+    if (!timeout) {
+        timeout = setTimeout(() => {
+            clearTimeout(timeout);
+            timeout = null;
+            if (this.scrollTop < 600) {
+                const currentHeight = $messages[0].scrollHeight;
+                const currentScroll = $messages.scrollTop();
+                prependMessages("public", 50, loadedMessages).then(
+                    (messages) => {
+                        const newHeight = $messages[0].scrollHeight;
 
+                        $messages.scrollTop(
+                            newHeight - currentHeight + currentScroll
+                        );
+                        loadedMessages += messages.length;
+                    }
+                );
+            }
+        }, 250);
+    }
+}
